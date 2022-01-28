bde10561f477992782f6a5269430c23e726b3e3d
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-27 20:05:03 -0600

    Write event listner to capture text that is typed into text area and speak text aloud. Write both functions respectively

--- script.js
+++ script.js
@@ -126,7 +126,15 @@ toggleBtn.addEventListener('click', () => document.getElementById('text-box').cl
 // Close button
 closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
 
+// Change voice
 voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
 getVoices();
 
 
