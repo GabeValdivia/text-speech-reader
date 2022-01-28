df763c473e3f63b287ac246facb0caace4271eda
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-27 20:00:12 -0600

    Add event listenter to setVoice() on change, write setVoice() function to change voice on change event

--- script.js
+++ script.js
@@ -112,6 +112,11 @@ function speakText() {
   speechSynthesis.speak(message);
 }
 
+// Set voice
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
 // Voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices);
 
@@ -121,4 +126,7 @@ toggleBtn.addEventListener('click', () => document.getElementById('text-box').cl
 // Close button
 closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
 
+voicesSelect.addEventListener('change', setVoice);
 getVoices();
+
+
