c5043083920149bee223582abe51d5641618c404
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-27 19:15:57 -0600

    Create an empty array to store voices list. Write function to grab voices from Googles speechSynthesis API, appendChild to 'option' element in text box

--- script.js
+++ script.js
@@ -76,8 +76,29 @@ function createBox(item) {
 
 }
 
+// Store voices
+let voice = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
 // Toggle text box
 toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
 
-// Toggle text box
-closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+// Close button
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+getVoices();
